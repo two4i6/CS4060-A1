02c85490cabff804ac82457e31f1d7286a04d258
Terry <terry@TerrydeMacBook-Pro.local>
2022-09-19 17:25:47 -0300

    finish

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider, Box } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <ChakraProvider>
+      <Box bg={'black'}>
+        <Component {...pageProps} />
+      </Box>
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,64 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import nc from "next-connect";
+import {prisma, User} from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
 
-type Data = {
-  name: string
+// a singleton instance of the database client
+class DatabaseClient {
+    private static instance: DatabaseClient;
+    private dbclient: PrismaClient;
+    
+    private constructor() {
+        this.dbclient = new PrismaClient()
+    }
+
+    get client():PrismaClient { return this.dbclient;}
+
+    public static getInstance(): DatabaseClient {
+        if (!DatabaseClient.instance) {
+            DatabaseClient.instance = new DatabaseClient();
+        }
+        return DatabaseClient.instance;
+    }
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+const dbclient = DatabaseClient.getInstance().client;
+
+interface ExtendedRequest extends NextApiRequest {
+  body: User
 }
+
+const logger = (req: NextApiRequest, res: NextApiResponse, next: Function) => {
+  console.log('req: ' + req.method, req.url, req.headers['x-forwarded-for'] || req.socket.remoteAddress )
+  next()
+}
+const handler = nc<NextApiRequest, NextApiResponse>({
+  onError(error, req, res) {
+      res.status(500).json({ error: `Sorry something Happened! ${error.message}` });
+  },
+  onNoMatch(req, res) {
+      res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
+  },
+})
+.use(logger)
+.post(async(req:ExtendedRequest, res)=>{
+  let newUser:User
+  try{
+    newUser= await dbclient.user.create({
+      data:req.body,
+    })
+    res.status(200).json({success:`user ${newUser.email} registered`})
+  }catch(e){
+    await dbclient.user.findMany({
+      where: {
+          email: {
+              contains: req.body.email
+          }
+      }
+    }) !== null ? res.status(406).json({error: 'user exits'}) :
+    res.status(418).json({error: e})
+  }
+})
+
+
+export default handler;
